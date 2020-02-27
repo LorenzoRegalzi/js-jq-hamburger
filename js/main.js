bf73dc6ee00cf26ecf01bed8457e4b892811410f
l <lorenzo.regalzi@gmail.com>
2020-02-27 23:13:12 +0100

    prova

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+$('#mario').mouseenter(function() {
+    $('#mario').addClass('colore')
+})
+$('#mario').mouseleave(function() {
+    $('#mario').removeClass('colore')
+})
+
+// var elemento= document.getElementById('mario');
+//
+// console.log(document.getElementById('mario').className);
+// console.log(elemento.className);
+//
+// elemento.addEventListener('click', function(){
+//     alert('BOOM');
+// });
